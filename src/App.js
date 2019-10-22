0faf3246d3b734de0ab424dec548b86332d46711
Walter_Porzel <walterp@vt.edu>
2019-10-22 16:19:58 -0400

    added hompage

--- src/App.js
+++ src/App.js
@@ -5,14 +5,18 @@ import Foods from './Foods'
 import FoodDetail from './FoodDetail'
 import NewFood from './NewFood';
 import UpdateFood from './UpdateFood';
+import Intro from './Intro';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Nutrition4U</h1>
-      <nav>
-        <Link to="/foods">Food List</Link>
-      </nav>
+    <div>
+      <div className='head'>
+        <Link to='/' className='title'><h1>Nutrition4U</h1></Link>
+        <nav>
+          <Link to="/foods" className='navbarLinks'>Food List</Link>
+        </nav>
+      </div>
+      <Route path='/' exact component = {Intro} />
       <Route path="/foods" exact component = {Foods} />
       <Route path="/foods/:id" exact component = {FoodDetail} />
       <Route path="/newFood" exact component = {NewFood} />
---
+++ src/Intro.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Intro(){
+    return(
+        <div className='wrapper'>
+        <p>You are what you eat! And if thats the case, you better know exactly what’s in whatever your eating. Search through our extensive database of foods to find the nutritional breakdown of whatever you’re about to eat! If you don’t see your food or something looks incorrect, feel free to update our database, we appreciate the help :)</p>
+        <img src='https://media.mnn.com/assets/images/2017/10/food_pyramid_visualize.jpg.653x0_q80_crop-smart.jpg' />
+        </div>
+    )
+}
+export default Intro
